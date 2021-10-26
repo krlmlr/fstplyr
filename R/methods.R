188784859254c2b412fc781fd05e7a7a53a65e17
Bobbitt <bobbits1@horacemann.com>
2021-10-26 13:24:38 -0500

    Update slice method

--- R/methods.R
+++ R/methods.R
@@ -82,7 +82,24 @@ setequal.tbl_fst <- function(x, y, ...) {
 dplyr::slice
 #' @export
 slice.tbl_fst <- function(.data, ...) {
-  slice(collect(.data), ...)
+
+  .dots <- list(...)
+
+  stopifnot(length(.dots) == 1L)
+
+  if (all(.dots[[1]] > 0)) {
+
+    collect(.data,
+            slice = .dots[[1]])
+
+  } else if (all(.dots[[1]] < 0)) {
+
+    collect(.data,
+            slice = setdiff(1:dim(.data)[1],
+                            -.dots[[1]]))
+
+  } else stop("Row indices must be exclusively positive or negative.")
+
 }
 
 #' @export

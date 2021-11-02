f8cdff2e036db7c64dba85b906f61d556d70b08b
Bobbitt <bobbits1@horacemann.com>
2021-11-02 10:14:17 -0500

    Move slice endorsements to prodctr.

--- R/slice.R
+++ R/slice.R
@@ -21,33 +21,3 @@ slice.tbl_fst <- function(.data, ...) {
   } else stop("Row indices must be exclusively positive or negative.")
 
 }
-
-#' @export
-slice_endorsements <- function(.data,
-                               effective = "1900-01-01",
-                               expiry = "9000-12-31",
-                               include_inforce = TRUE) {
-
-  dates_dt <- collect(select(.data,
-                             ETL_END_EFF_DTS,
-                             ETL_END_EXP_DTS))
-
-  if (!include_inforce) {
-
-    index <- data.table:::`[.data.table`(dates_dt,
-                                         ETL_END_EFF_DTS >= effective &
-                                           ETL_END_EXP_DTS < expiry,
-                                         which = TRUE)
-
-  } else if (include_inforce) {
-
-    index <- data.table:::`[.data.table`(dates_dt,
-                                         ETL_END_EXP_DTS > effective &
-                                           ETL_END_EFF_DTS < expiry,
-                                         which = TRUE)
-
-  } else stop("include_inforce must be a logical.")
-
-  slice(.data, index)
-
-}
